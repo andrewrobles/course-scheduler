d8b4f68c327e6426cced83c5c98c3354b6cb857f
Andrew Robles <andrewrobles@verizon.net>
2022-01-22 04:08:04 -0800

    Add schedule serializer

--- api/api/service/views.py
+++ api/api/service/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from api.service.serializers import UserSerializer, GroupSerializer
+from api.service.serializers import UserSerializer, GroupSerializer, ScheduleSerializer
 
 from django.shortcuts import get_object_or_404
 from .models import Schedule
@@ -78,3 +78,13 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class ScheduleViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows schedules to be viewed or edited.
+    """
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
